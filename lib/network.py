fde6a532a3b0b30881885033c7639832a5fcb901
Restyled.io <commits@restyled.io>
2020-09-21 06:32:30 +0000

    Restyled by black

--- lib/network.py
+++ lib/network.py
@@ -14,17 +14,28 @@ from pathlib import Path as pathlib_Path
 from zenipy.zenipy import entry as entry_token, error as Error
 from pgrep import pgrep as check_process
 
-BLUE, RED, WHITE, CYAN, DEFAULT, YELLOW, MAGENTA, GREEN, END, BOLD = '\33[94m', '\033[91m', '\33[97m', '\033[36m', '\033[0m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m', '\033[1m'
+BLUE, RED, WHITE, CYAN, DEFAULT, YELLOW, MAGENTA, GREEN, END, BOLD = (
+    "\33[94m",
+    "\033[91m",
+    "\33[97m",
+    "\033[36m",
+    "\033[0m",
+    "\33[93m",
+    "\033[1;35m",
+    "\033[1;32m",
+    "\033[0m",
+    "\033[1m",
+)
 
 
 def local():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(('10.255.255.255', 1))
+        s.connect(("10.255.255.255", 1))
         IP = s.getsockname()[0]
         print("\n{0}Local IP: {1}{2}".format(GREEN, DEFAULT, IP))
     except:
-        IP = '127.0.0.1'
+        IP = "127.0.0.1"
     finally:
         s.close()
     return IP
@@ -48,11 +59,11 @@ def run_ngrok():
         for p in pid:
             kill_process(p, signal.SIGKILL)
         # Continue
-        run_command(
-            './ngrok tcp -config=.config/ngrok.yml 443 > /dev/null 2>&1 &')
+        run_command("./ngrok tcp -config=.config/ngrok.yml 443 > /dev/null 2>&1 &")
         while True:
             tcp = sys_url(
-                'curl -s -N http://127.0.0.1:4040/status | grep -o "tcp://[0-9]*.tcp.ngrok.io:[0-9]*"').read()
+                'curl -s -N http://127.0.0.1:4040/status | grep -o "tcp://[0-9]*.tcp.ngrok.io:[0-9]*"'
+            ).read()
             if re.match("tcp://[0-9]*.tcp.ngrok.io:[0-9]*", tcp) != None:
                 print("\n{0}Ngrok TCP: {1}{2}".format(GREEN, DEFAULT, tcp))
                 break
@@ -60,13 +71,17 @@ def run_ngrok():
         while True:
             try:
                 token = entry_token(
-                    title="SET NGROK AUTHTOKEN", text="Register at https://ngrok.com\n", width=450, height=140)
+                    title="SET NGROK AUTHTOKEN",
+                    text="Register at https://ngrok.com\n",
+                    width=450,
+                    height=140,
+                )
                 if not len(token) in range(40, 50):
                     Error(text="Invalid token, please try again")
                     continue
                 else:
                     ngrok_config.touch(mode=0o777, exist_ok=True)
-                    ngrok_config = open('.config/ngrok.yml', 'w')
+                    ngrok_config = open(".config/ngrok.yml", "w")
                     ngrok_config.write("authtoken: " + token)
                     ngrok_config.close()
                     run_ngrok()
